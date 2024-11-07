2b8c5234e2c188a62cd2ce401f9ca18ab722d409
KrishKhattar <krish.khattar@gmail.com>
2024-11-08 04:28:49 +0530

    added icon

--- dataPair.js
+++ dataPair.js
@@ -1,20 +1,19 @@
 function getUserProfilePairsAsJSON(namesArray, linksArray) {
-    const pairsArray = [];
-  
-    // Create the array of name-link pairs as objects
-    for (let i = 0; i < Math.min(namesArray.length, linksArray.length); i++) {
-      pairsArray.push({
-        name: namesArray[i],
-        link: linksArray[i],
-      });
-    }
-  
-    // Convert the array of objects to JSON
-    const jsonPairs = JSON.stringify(pairsArray, null, 2);
-  
-    // Print the JSON to the console
-    console.log(jsonPairs);
-  
-    return jsonPairs;
+  const pairsArray = [];
+
+  // Create the array of name-link pairs as objects
+  for (let i = 0; i < Math.min(namesArray.length, linksArray.length); i++) {
+    pairsArray.push({
+      name: namesArray[i],
+      profile: linksArray[i],
+    });
   }
-  
+
+  // Convert the array of objects to JSON
+  const jsonPairs = JSON.stringify(pairsArray, null, 2);
+
+  // Print the JSON to the console
+  console.log(jsonPairs);
+
+  return jsonPairs;
+}
--- linkedinLinks.js
+++ linkedinLinks.js
@@ -5,19 +5,19 @@ function getFilteredUniqueLinksArray() {
   // Create an array to store filtered unique links
   const filteredUniqueLinksArray = [];
 
-  const excludedLinkedInUrls = [
-    "https://www.linkedin.com/feed/",
-    "https://www.linkedin.com/mynetwork/",
-    "https://www.linkedin.com/jobs/",
-    "https://www.linkedin.com/messaging/",
-    "https://www.linkedin.com/notifications/",
-    "https://www.linkedin.com/sales",
-    "https://www.linkedin.com/search/results/people/",
-    "https://www.linkedin.com/talent/job-posting-redirect",
-    "https://www.linkedin.com/premium/products/",
-    "https://www.linkedin.com/company/98046425/admin/inbox",
-    "https://www.linkedin.com/search/results/people/headless",
-  ];
+//   const excludedLinkedInUrls = [
+//     "https://www.linkedin.com/feed/",
+//     "https://www.linkedin.com/mynetwork/",
+//     "https://www.linkedin.com/jobs/",
+//     "https://www.linkedin.com/messaging/",
+//     "https://www.linkedin.com/notifications/",
+//     "https://www.linkedin.com/sales",
+//     "https://www.linkedin.com/search/results/people/",
+//     "https://www.linkedin.com/talent/job-posting-redirect",
+//     "https://www.linkedin.com/premium/products/",
+//     "https://www.linkedin.com/company/98046425/admin/inbox",
+//     "https://www.linkedin.com/search/results/people/headless",
+//   ];
 
   // Regex to match LinkedIn profile links in the form 'https://www.linkedin.com/in/username' (username without 'ACoA' format)
   const linkedinProfileRegex =
